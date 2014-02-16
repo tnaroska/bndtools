a33621aee79fe667f04283af992347ee1a460444
Ferry Huberts <ferry.huberts@pelagic.nl>
2014-02-16 20:23:27 +0100

    gradle: build the projects with Gradle (v8)
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

---
+++ cnf/gradle/doc/makesvg.bat
@@ -0,0 +1,10 @@
+REM
+REM WARNING
+REM
+REM This file is maintained automatically by Bndtools.
+REM You should not edit it.
+REM
+REM See the BUILDING-GRADLE.md document in the root project for more details.
+REM
+
+dot -Tsvg "%1.dot" -o "%1.svg"
---
+++ cnf/gradle/doc/makesvg.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#
+# WARNING
+#
+# This file is maintained automatically by Bndtools.
+# You should not edit it.
+#
+# See the BUILDING-GRADLE.md document in the root project for more details.
+#
+
+
+dot -Tsvg "$1.dot" -o "$1.svg"
